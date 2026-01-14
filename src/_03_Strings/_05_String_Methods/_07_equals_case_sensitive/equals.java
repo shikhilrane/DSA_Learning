8139e9aae6d4d72b8de66f795198c2bd9475d7f3
Shikhil Rane <shikhilrane04@gmail.com>
2026-01-14 18:42:12 +0530

    added string

---
+++ src/_03_Strings/_05_String_Methods/_07_equals_case_sensitive/equals.java
@@ -0,0 +1,9 @@
+package _03_Strings._05_String_Methods._07_equals_case_sensitive;
+
+public class equals {
+    public static void main(String[] args) {
+        String fName = "Shikhil";
+        String lName = "SHIKHIL";
+        System.out.println(fName.equals(lName));    // It checks if values of StringClass are equal or not (it is case sensitive) o/p => true/false
+    }    
+}
---
+++ src/_03_Strings/_05_String_Methods/_21_copyValueOf_charArray_to_String/copyValueOf.java
@@ -0,0 +1,11 @@
+package _03_Strings._05_String_Methods._21_copyValueOf_charArray_to_String;
+
+// Use to convert character array into StringClass
+
+public class copyValueOf {
+    public static void main(String[] args) {
+        char c[] = {'H','e','l','l','o',' ','W','o','r','l','d'};
+        String newString = String.copyValueOf(c);
+        System.out.println(newString);
+    }    
+}

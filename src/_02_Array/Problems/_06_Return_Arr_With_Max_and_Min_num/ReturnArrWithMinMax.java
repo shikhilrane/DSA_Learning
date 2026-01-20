2fb0e4be2d4bed21fbf44e63b8778a7376b0870b
Shikhil Rane <shikhilrane04@gmail.com>
2026-01-20 18:46:14 +0530

    Added alternate method for Return Max and Min

--- src/_02_Array/Problems/_06_Return_Arr_With_Max_and_Min_num/ReturnArrWithMinMax.java
+++ src/_02_Array/Problems/_06_Return_Arr_With_Max_and_Min_num/ReturnArrWithMinMax.java
@@ -11,11 +11,35 @@ public class ReturnArrWithMinMax {
         int[] newArray = returnMaxMin(arr);
         System.out.println(newArray[0]);
         System.out.println(newArray[1]);
+
+        returnMinAndMax(arr);
     }
 
     public static int[] returnMaxMin(int[] a){
         Arrays.sort(a);                         // sort the array first
         int[] array = {a[0],a[a.length-1]};     // save first element and last element in array
         return array;                           // return array
+    }
+
+    public static void returnMinAndMax(int[] a) {
+        if (a.length == 0) {
+            System.out.println("Array is empty");
+            return;
+        }
+
+        int min = a[0];
+        int max = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+
+        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
     }
 }
